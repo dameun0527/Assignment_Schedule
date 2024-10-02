47f424c56a9460dd207993311b415118d44fbf4f
Jisoo.R <jisuryu0527@gmail.com>
2024-10-02 17:35:07 +0900

    Initial commit with assignment project

---
+++ src/test/java/com/sparta/assignment_schedule/AssignmentScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.assignment_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
